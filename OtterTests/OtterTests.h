4740d7377d68954cb89cff42f04b8dec50a9cb4b
Alex Onsager <alex.onsager@gmail.com>
2012-10-09 23:28:34 +0900

    Initial commit

---
+++ OtterTests/OtterTests.h
@@ -0,0 +1,13 @@
+//
+//  OtterTests.h
+//  OtterTests
+//
+//  Created by Onsager Alex on 10/9/12.
+//  Copyright (c) 2012 Onsager Alex. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OtterTests : SenTestCase
+
+@end
---
+++ OtterTests/OtterTests.m
@@ -0,0 +1,32 @@
+//
+//  OtterTests.m
+//  OtterTests
+//
+//  Created by Onsager Alex on 10/9/12.
+//  Copyright (c) 2012 Onsager Alex. All rights reserved.
+//
+
+#import "OtterTests.h"
+
+@implementation OtterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OtterTests");
+}
+
+@end
